137ea903e43e1073646b2075587ff237b5b5cd5a
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2020-02-21 10:39:37 +0900

    version 1.2

--- kill_process.py
+++ kill_process.py
@@ -3,47 +3,49 @@
 
 import sys
 import subprocess
+import time
+
+def getProcess():
+    process = subprocess.check_output('ps -h', shell=True)
+    process = process.split('\n')
+    ps_list = []
+    for i in range(len(process)-1):
+        ps_list.append(process[i].split())
+    return ps_list
 
 def killAll():
     print ('all')
-    process = subprocess.check_output('ps -h', shell=True)
-    process = process.split('\n')
-    ps_list = []
-    for i in range(len(process)-1):
-        ps_list.append(process[i].split())
+    ps_list = getProcess()
+    for i in range(len(ps_list)):
         if 'kill_process.py' in ps_list[i][-2]:
             break
         if ps_list[i][4] != 'bash':
-            subprocess.call('kill -15 '+ps_list[i][0], shell=True)
+            subprocess.call('kill -15 ' + ps_list[i][0], shell=True)
     loop_flg = True
     while loop_flg:
+        ps_list = getProcess()
         state_list = []
-        process = subprocess.check_output('ps -h', shell=True)
-        for i in range(len(process.split('\n'))-1):
-            state_list.append(process.split('\n')[i].split()[2])
+        for i in range(len(ps_list)):
+            state_list.append(ps_list[i][2])
         if not('Sl+' in state_list):
             loop_flg = False
-    process = subprocess.check_output('ps -h', shell=True)
-    process = process.split('\n')
-    id_list = []
-    for i in range(len(process)-1):
-        id_list.append(process[i].split()[0])
-        if 'kill_process.py' in process[i].split()[-1]:
+        time.sleep(0.1)
+    ps_list = getProcess()
+    for i in range(len(ps_list)):
+        if 'kill_process.py' in ps_list[i][-2]:
             break
-        subprocess.call('sudo kill -9 '+id_list[i], shell=True)
+        subprocess.call('sudo kill -9 ' + ps_list[i][0], shell=True)
 
 def killRuning():
     print ('runing')
-    process = subprocess.check_output('ps -h', shell=True)
-    process = process.split('\n')
-    ps_list = []
-    for i in range(len(process)-1):
-        ps_list.append(process[i].split())
+    ps_list = getProcess()
+    for i in range(len(ps_list)):
         if 'kill_process.py' in ps_list[i][-2]:
             break
         if ps_list[i][4] != 'bash':
-            subprocess.call('kill -2 '+ps_list[i][0], shell=True)
-        
+            subprocess.call('kill -2 ' + ps_list[i][0], shell=True)
+
+            
 if __name__ == '__main__':
     args = sys.argv
     try:
